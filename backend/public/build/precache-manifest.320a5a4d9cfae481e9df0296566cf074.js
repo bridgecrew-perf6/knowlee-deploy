71f8e1cf30e1d4e4d34ab2cccfd675e8e3add2d8
Leonardo Campuzano Magallanes <leonardocampuzano43@gmail.com>
2020-12-11 12:39:17 -0600

    + build

---
+++ backend/public/build/precache-manifest.320a5a4d9cfae481e9df0296566cf074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a364500be2d9f80b1067cf99dbd4cad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7746f2449aa7b2527ac4",
+    "url": "/static/css/2.9d6f32cf.chunk.css"
+  },
+  {
+    "revision": "a826b0d7856f15b006e8",
+    "url": "/static/css/main.023496d0.chunk.css"
+  },
+  {
+    "revision": "7746f2449aa7b2527ac4",
+    "url": "/static/js/2.05edb74e.chunk.js"
+  },
+  {
+    "revision": "a826b0d7856f15b006e8",
+    "url": "/static/js/main.7962d96f.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
